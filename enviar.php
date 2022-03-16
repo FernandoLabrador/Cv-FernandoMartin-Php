3a9d73ea7e1154a3204a2f209fe1deaf5b3e4ee2
Fernando Labrador <Labradorfm93@gmail.com>
2022-03-16 11:10:45 -0300

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,16 +1,56 @@
-<?php 
-// Llamando a los campos
-$nombre = $_POST['nombre'];
-$correo = $_POST['correo'];
-$mensaje = $_POST['mensaje'];
-// Datos para el correo
-$destinatario = "labradorfm93@gmail.com";
-$asunto = "Contacto desde nuestra web";
-$carta = "De: $nombre \n";
-$carta .= "Correo: $correo \n";
-$carta .= "Mensaje: $mensaje";
-// $carta .= "Enviado el " .date('d/m/Y', time());
-// Enviando Mensaje
-mail($destinatario, $asunto, $carta);
-header('Location:mensaje-de-envio.html');
+<?php
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+$name = $_POST['nombre'];
+$email = $_POST['correo'];
+$comment = $_POST['mensaje'];
+
+
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output SMTP::DEBUG_SERVER
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.office365.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = $_ENV['MAILER_USER'];                     //SMTP username
+    $mail->Password   = $_ENV['MAILER_PASWORD'];                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom( $_ENV['MAILER_USER'], 'Mailer');
+    $mail->addAddress( $_ENV['MAILER_FROM'], 'fernando');     //Add a recipient
+    //$mail->addAddress('ellen@example.com');               //Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Mensaje de Probemo otro';
+    $mail->Body    =  'Nombre: ' . $name . '<br/>Correo: ' . $email . '<br/>' . $comment;
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo header('Location:mensaje-de-envio.html');
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+
+
 ?>
